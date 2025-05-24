c60caaf274cb1c36b50df640813ed48fb7bf40b0
Sümeyye Çekiç <sumicekic@gmail.com>
2025-05-24 22:04:00 +0300

    ucus ve lokasyona tostring metodu eklendi /  okunabilirlik artırıldı.

--- lokasyon.java
+++ lokasyon.java
@@ -1,11 +1,18 @@
-public class lokasyon {
-  String ulke, sehir , havaalani;
-  boolean aktif; 
-  
-public Lokasyon(String ulke, String sehir, String havaalani, boolean aktif) {
-this.ulke = ulke;
-this.sehir = sehir;
-this.havaalani = havaalani;
-this.aktif = aktif;
+    public class lokasyon {
+    String ulke, sehir , havaalani;
+    boolean aktif; 
+
+    public Lokasyon(String ulke, String sehir, String havaalani, boolean aktif) {
+    this.ulke = ulke;
+    this.sehir = sehir;
+    this.havaalani = havaalani;
+    this.aktif = aktif;
     
 }
+    @Override
+    public String toString() {
+        return sehir + ", " + ulke + " - " + havaalani;
+    }
+}
+
+
--- ucus.java
+++ ucus.java
@@ -4,12 +4,23 @@ public class ucus {
     Ucak ucak;
     int kalanKoltuk; 
 
-public Ucus(Lokasyon kalkis, Lokasyon varis, String saat, Ucak ucak) {
-this.kalkis = kalkis;
-this.varis = varis;
-this.saat = saat;
-this.ucak = ucak;
-this.kalanKoltuk = ucak.getKapasite();
+    public Ucus(Lokasyon kalkis, Lokasyon varis, String saat, Ucak ucak) {
+    this.kalkis = kalkis;
+    this.varis = varis;
+    this.saat = saat;
+    this.ucak = ucak;
+    this.kalanKoltuk = ucak.getKapasite();
 }
 
+    public boolean rezervasyonYap() {
+        if (kalanKoltuk > 0) {
+            kalanKoltuk--;
+            return true;
+        }
+        return false;
+    }
+@Override
+    public String toString() {
+        return kalkis + " -> " + varis + " | Saat: " + saat + " | Uçak: " + ucak;
+    }
 }
